0bc6b350e16c24418ad435353aaf950f6349f5ce
Evgeniy <evgeniy.borisov@yahoo.com>
2023-10-22 13:53:45 +0300

    add models users

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_executor = models.BooleanField(default=False)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    photo_user = models.ImageField()
+    name = models.CharField(max_length=100, null=True, blank=True)
+    birhday = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()

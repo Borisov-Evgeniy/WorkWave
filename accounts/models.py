4277a17741511683146afbe15af893f733454bbc
Evgeniy <evgeniy.borisov@yahoo.com>
2023-10-27 15:56:17 +0300

    upd models

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
     is_customer = models.BooleanField(default=False)
     is_executor = models.BooleanField(default=False)
+    groups = models.ManyToManyField(Group, verbose_name=("groups"), blank=True, related_name='customuser_groups')
+    user_permissions = models.ManyToManyField(Permission, verbose_name=("user permissions"),blank=True,related_name='customuser_user_permissions')
 
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
@@ -11,3 +13,6 @@ class UserProfile(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     birhday = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name

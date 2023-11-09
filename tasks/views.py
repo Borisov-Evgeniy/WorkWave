afd10623214e5fd2154a0b88907ca41222127dd4
Evgeniy <evgeniy.borisov@yahoo.com>
2023-11-09 21:18:30 +0300

    upd view task

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import CreateTask
 
+@login_required
 def create_task(request):
     if request.method == 'POST':
         create_task_form = CreateTask(request.POST, request.FILES)
         if create_task_form.is_valid():
-            task = create_task_form.save(commit=False)
-            task.user = request.user  # привязка задания к текущему пользователю
-            task.save()
-            return redirect('home')
+            try:
+                task = create_task_form.save(commit=False)
+                task.user = request.user  # привязка задания к текущему пользователю
+                task.save()
+                return redirect('home')
+            except Exception as e:
+                return HttpResponseServerError(f"Error while saving user: {e}")
     else:
         create_task_form = CreateTask()
 

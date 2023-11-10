bc4a688cedd73ecadc2b6868ca4a30cb20ac16ef
Evgeniy <evgeniy.borisov@yahoo.com>
2023-11-10 16:04:37 +0300

    add task_list templates and pagination

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from tasks import views as view_tasks
 
 urlpatterns = [
 
-    path('create_task/', views.create_task, name='create_task'),
-    path('task_list/', views.open_create_task_modal, name='task_list'),
+    path('create_task/', view_tasks.create_task, name='create_task'),
+    path('task_list/', view_tasks.task_list, name='task_list'),
+    path('take_task/<int:task_id>/', view_tasks.take_task, name='take_task'),
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import CreateTask
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator
+from .models import Task
+from django.http import JsonResponse
+
 
 @login_required
 def create_task(request):
@@ -9,12 +15,32 @@ def create_task(request):
             try:
                 task = create_task_form.save(commit=False)
                 task.user = request.user  # привязка задания к текущему пользователю
+                task.customer = request.user  # установка заказчика
                 task.save()
                 return redirect('home')
             except Exception as e:
-                return HttpResponseServerError(f"Error while saving user: {e}")
+                return HttpResponseServerError(f"Error while saving task: {e}")
     else:
         create_task_form = CreateTask()
 
-    return render(request, 'accounts/index.html', {'create_task_form': create_task_form})
+    return render(request, 'tasks/create_task.html', {'create_task_form': create_task_form})
 
+def task_list(request):
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 12)  # 12 задач на странице
+    page = request.GET.get('page')
+    tasks = paginator.get_page(page)
+
+    context = {'tasks': tasks}
+    return render(request, 'tasks/task_list.html', context)
+
+@require_POST
+def take_task(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+        task.executor = request.user  # Предполагается, что пользователь авторизован
+        task.save()
+        return JsonResponse({'message': 'Задание успешно взято'})
+    except Task.DoesNotExist:
+        return JsonResponse({'message': 'Задание не найдено'}, status=404)
